f91a9a18b5003e30eb07e2d27060bf409ad14db4
Francisco <Francisco@DESKTOP-7U6N0QM>
2023-03-05 22:50:32 +0100

    entero_a_4

--- src/francisco_pomata/git/Francisco_PomataGIT.java
+++ src/francisco_pomata/git/Francisco_PomataGIT.java
@@ -7,7 +7,7 @@ public class Francisco_PomataGIT {
 
     public static void main(String[] args) {
         int entero;
-        entero = 3;
+        entero = 4;
         System.out.println("Este es un mensaje para realizar pruebas, ahí va un número: " + entero);
     }
 
