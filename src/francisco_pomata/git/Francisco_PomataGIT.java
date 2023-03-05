786c954637a3bea289fd90d7e5f6b4c5a1bfe4a2
Francisco <Francisco@DESKTOP-7U6N0QM>
2023-03-05 22:24:07 +0100

    entero_a_2

--- src/francisco_pomata/git/Francisco_PomataGIT.java
+++ src/francisco_pomata/git/Francisco_PomataGIT.java
@@ -1,24 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package francisco_pomata.git;
 
 /**
- *
- * @author Francisco Pomata Lozano
- *         Tarea Entornos de Desarrollo ED04-2
+  * @author Francisco Pomata Lozano Tarea Entornos de Desarrollo ED04-2
  */
 public class Francisco_PomataGIT {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        
         int entero;
-        entero = 1;
-        System.out.println("Este es un mensaje para realizar pruebas, ahí va un número: "+entero);
+        entero = 2;
+        System.out.println("Este es un mensaje para realizar pruebas, ahí va un número: " + entero);
     }
-    
+
 }
